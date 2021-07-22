512e157b6db335b6b48448ce07871208cd494240
Nageswar Padal <npadal848@gmail.com>
2021-07-23 01:45:42 +0530

    Added stream practice problem

--- src/stream/OccuranceOfCharInString.java
+++ src/stream/OccuranceOfCharInString.java
@@ -5,23 +5,20 @@ import java.util.stream.Collectors;
 
 public class OccuranceOfCharInString {
 
+	static char ch;
+
 	public static void main(String[] args) {
 		String str = "Java";
-		
-//		First Method
-		Map<Character, Integer> map= str.chars().boxed().collect(Collectors
-				.toMap(key-> Character.valueOf((char)key.intValue()),
-				value-> 1, Integer::sum));
+
+		// First Method
+		Map<Character, Integer> map = str.chars().boxed()
+				.collect(Collectors.toMap(key -> Character.valueOf((char) key.intValue()), value -> 1, Integer::sum));
 		System.out.println(map);
-		
+
 //		Second Method
-		Map<Character, Integer> occuranceMap = str.chars()
-				.boxed()
-				.collect(Collectors.toMap(
-						k-> Character.valueOf((char) k.intValue()), 
-						v -> 1, 
-						Integer::sum));
-		
+		Map<Character, Integer> occuranceMap = str.chars().boxed()
+				.collect(Collectors.toMap(k -> Character.valueOf((char) k.intValue()), v -> 1, Integer::sum));
+
 		System.out.println(occuranceMap);
 	}
 }
